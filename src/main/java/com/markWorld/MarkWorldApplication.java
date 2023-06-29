17f5506d7d53ab852f5afe41756b73ab7953e0e0
kedom <kedom7@163.com>
2023-06-29 22:13:05 +0800

    SpringBoot项目搭建完成

---
+++ src/main/java/com/markWorld/MarkWorldApplication.java
@@ -0,0 +1,11 @@
+package com.markWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MarkWorldApplication.class, args);
+    }
+}
